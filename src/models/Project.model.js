cdb8214ed4ccd31209b5780794dc0be8f27b1282
문형석 <hyn9xc@gmail.com>
2023-08-29 21:41:44 +0900

    refactor: change schemas from embedding to referencing

--- src/models/Project.model.js
+++ src/models/Project.model.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 
 const pathSchema = new mongoose.Schema({
-  unicode: { type: Number, required: true, unique: true, immutable: true },
-  pathString: {
-    type: String,
-    required: true,
-    unique: true,
-    immutable: true,
-  },
+  unicode: { type: Number, required: true, immutable: true },
+  pathString: { type: String },
 });
 
 const projectSchema = new mongoose.Schema(
   {
     name: { type: String },
-    unicodePaths: { type: [pathSchema] },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    unicodePaths: {
+      type: [pathSchema],
+      default: Array.from({ length: 94 }, (_, index) => ({
+        unicode: index + 33,
+        pathString: "",
+      })),
+    },
   },
   { timestamps: true }
 );
